5f5bb3c33057e1bdb30832c674c454709e993734
annmerin <25018023+annmerin@users.noreply.github.com>
2022-01-14 21:47:22 -0800

    add three numbers

--- oddNumberMagic.py
+++ oddNumberMagic.py
@@ -6,6 +6,8 @@ def oddnumbermagic(size):
     print(size)
     msquare = np.zeros((int(size), int(size)))
     print(msquare)
+    max = int(size)*int(size)
+    print(max)
     fno = int(size) / 2
     fno = math.floor(fno) # to get the x position of the first number
     # add the first number
@@ -18,24 +20,29 @@ def oddnumbermagic(size):
     index_y -= 1
     index_x +=1
     print(index_y, index_x)
-    if index_y < 0 and index_x <= int(size) - 1:
+    print(int(size))
+    if index_y < 0 and index_x < int(size):
         index_y = int(size) - 1
         msquare[index_y,index_x] = no
         print(msquare)
-        print(index_y, index_x)
         no += 1
-        print(no)
         index_y -= 1
         index_x += 1
-    elif index_y > 0 and index_x > int(size) - 1:
+        print(index_y, index_x)
+        max -= 1
+    if index_y > 0 and index_x > int(size) - 1:
         index_x = 0
         print(index_y,index_x)
         msquare[index_y,index_x] = no
+        print(msquare)
         no += 1
+        max -= 1
     else:
         print(index_y, index_x)
         msquare[index_y, index_x] = no
         print(msquare)
+        no += 1
+        max -= 1
 
 
 
