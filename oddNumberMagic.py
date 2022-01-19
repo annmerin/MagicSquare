a0f329d13939d6a96a835fcf0231885f92bc86d4
annmerin <25018023+annmerin@users.noreply.github.com>
2022-01-18 21:37:22 -0800

    try standardize

--- oddNumberMagic.py
+++ oddNumberMagic.py
@@ -12,6 +12,7 @@ def oddnumbermagic(size):
     fno = math.floor(fno) # to get the x position of the first number
     # add the first number
     msquare[0,fno] = 1
+    max -= 1
     # add the second number
     no = 2
     # get the position of number 1 using where key
@@ -21,42 +22,63 @@ def oddnumbermagic(size):
     index_x +=1
     print(index_y, index_x)
     print(int(size))
-    if index_y < 0 and index_x < int(size):
-        index_y = int(size) - 1
-        msquare[index_y,index_x] = no
-        print(msquare)
-        no += 1
-        index_y -= 1
-        index_x += 1
-        print(index_y, index_x)
-        max -= 1
-    if index_y > 0 and index_x > int(size) - 1:
-        index_x = 0
-        print(index_y,index_x)
-        msquare[index_y,index_x] = no
-        print(msquare)
-        no += 1
-        max -= 1
-        index_y -= 1
-        index_x += 1
-    if index_y == 0 and index_x < int(size) - 1 and msquare[index_y,index_x] != 0:
-        index_y += 2
-        index_x -= 1
-        print(index_y, index_x)
-        msquare[index_y, index_x] = no
-        print(msquare)
-        no += 1
-        max -= 1
-        index_y -= 1
-        index_x += 1
-    else:
-        print(index_y, index_x)
-        msquare[index_y, index_x] = no
-        print(msquare)
-        no += 1
-        max -= 1
-        index_y -= 1
-        index_x += 1
+    while max > 1 :
+        if index_y < 0 and index_x < int(size):
+            index_y = int(size) - 1
+            msquare[index_y,index_x] = no
+            print(msquare)
+            no += 1
+            index_y -= 1
+            index_x += 1
+            print(index_y, index_x)
+            max -= 1
+        if index_y > 0 and index_x > int(size) - 1:
+            index_x = 0
+            print(index_y,index_x)
+            msquare[index_y,index_x] = no
+            print(msquare)
+            no += 1
+            max -= 1
+            index_y -= 1
+            index_x += 1
+     #   if index_y == 0 and index_x < int(size) - 1 and msquare[index_y,index_x] != 0:
+        if (index_y > 0 and index_y < int(size)-1) and (index_x > 0 and  index_x < int(size) - 1) and msquare[index_y, index_x] != 0:
+            index_y += 2
+            index_x -= 1
+            print(index_y, index_x)
+            msquare[index_y, index_x] = no
+            print(msquare)
+            no += 1
+            max -= 1
+            index_y -= 1
+            index_x += 1
+        if index_y < 0 and index_x == int(size):
+            index_y = 1
+            index_x -= 1
+            msquare[index_y,index_x] = no
+            print(msquare)
+            no += 1
+            index_y -= 1
+            index_x += 1
+            print(index_y, index_x)
+            max -= 1
+        if index_y == 0 and index_x == int(size):
+            index_x = 0
+            msquare[index_y, index_x] = no
+            print(msquare)
+            no += 1
+            index_y -= 1
+            index_x += 1
+            print(index_y, index_x)
+            max -= 1
+        else:
+            print(index_y, index_x)
+            msquare[index_y, index_x] = no
+            print(msquare)
+            no += 1
+            max -= 1
+            index_y -= 1
+            index_x += 1
 
 
 
